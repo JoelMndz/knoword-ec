8d8304391c88a6fe832907e79c83f5058b51e636
JoelMndz <luisjo3lml@gmail.com>
2024-11-30 21:56:57 -0500

    Subor .output

---
+++ .output/public/_nuxt/BgQvoqQB.js
@@ -0,0 +1,1 @@
+var e=(r=>(r.Estudiante="estudiante",r.Profesor="profesor",r))(e||{});export{e as R};
---
+++ .output/public/_nuxt/XqKEbuup.js
@@ -0,0 +1,1 @@
+const r=n=>{const o=n/60,t=n%60;return`${Math.trunc(o)}:${t==0?"00":t<10?"0"+t:t}`};export{r as f};
